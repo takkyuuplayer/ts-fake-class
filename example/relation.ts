7947078a8d613c91fe41acbaf5fc950384ba9452
takkyuuplayer <takkyuuplayer@gmail.com>
2020-03-07 21:21:20 -0500

    Add example

---
+++ example/relation.ts
@@ -0,0 +1,15 @@
+import { fakeClass, FakableClass, FakableField } from "../src/index";
+
+@FakableClass()
+class User {
+  @FakableField(() => 5, {
+    order: 0
+  })
+  public id?: number;
+
+  @FakableField((user: User) => `test-${user.id}@example.com`, { order: 1 })
+  public email?: string;
+}
+
+const user = fakeClass(User);
+console.log(user); // User {id: 5, email: 'test-5@example.com' }
